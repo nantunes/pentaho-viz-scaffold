e4ed0385d874184b9ffa703249d81922c14c06d6
Nelson Antunes <nelson.antunes@webdetails.pt>
2016-01-19 18:04:06 +0000

    Gets sandbox from github

--- src/gen/index.js
+++ src/gen/index.js
@@ -98,6 +98,6 @@ module.exports = generators.Base.extend({
   },
 
   install: function () {
-    this.npmInstall(['git+https://github.com/carlosrusso/pentaho-platform-plugin-common-ui.git#BACKLOG-6020', 'pentaho-viz-sandbox'], {'save-dev': true});
+    this.npmInstall(['git+https://github.com/carlosrusso/pentaho-platform-plugin-common-ui.git#BACKLOG-6020', 'https://github.com/nantunes/pentaho-viz-sandbox'], {'save-dev': true});
   }
 });
