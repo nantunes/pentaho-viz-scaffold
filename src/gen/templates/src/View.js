b2d01bd14337eb15c3af85e69b7a06c84a6ec2f7
Nelson Antunes <nelson.antunes@webdetails.pt>
2016-01-20 16:27:20 +0000

    Fixes

--- src/gen/templates/src/View.js
+++ src/gen/templates/src/View.js
@@ -1,21 +1,16 @@
 define([
   "pentaho/visual/base/View",
-  "pentaho/i18n!view"
+  "pentaho/i18n!View"
 ], function(Visual, bundle) {
   "use strict";
 
-  /*global document:true*/
+  /*global document:true, window:false*/
 
   /**
    * @name View
-   * @memberOf pentaho.visual.samples.calc
    * @class
-   * @classDesc The `View` of the calculator visualization.
    *
    * @description Creates a calculator `View`.
-   * @constructor
-   * @param {HTMLDOMElement} element The DOM element where the visualization should render.
-   * @param {pentaho.visual.samples.calc.Model} model The calculator's visualization `Model`.
    */
   return Visual.extend({
     /** @override */
@@ -26,18 +21,19 @@ define([
     /** @override */
     _render: function() {
       var result = this._calculate();
-      // TODO: format result
 
-      this.numSpan.innerHTML = result + " (I'm the dev viz)";
+      this.numSpan.innerHTML = bundle.get("dev_result", [result]);
 
-      this._resize();
+      var width = this.model.getv("width");
+      var height = this.model.getv("height");
+      this._resize(width, height);
     },
 
     /** @override */
     _resize: function(width, height) {
       // Center the span
-      this.numSpan.style.left = ((width  - this.numSpan.offsetWidth ) / 2) + "px";
-      this.numSpan.style.top  = ((height - this.numSpan.offsetHeight) / 2) + "px";
+      this.numSpan.style.width = width + "px";
+      this.numSpan.style.height  = height + "px";
     },
 
     /** @override */
@@ -53,44 +49,77 @@ define([
       this.numSpan = document.createElement("span");
       this.numSpan.style.fontSize = "42px";
       this.numSpan.style.position = "relative";
+      this.numSpan.style.display = "block";
+      this.numSpan.style.overflow = "scroll";
       this.element.appendChild(this.numSpan);
     },
 
     _calculate: function() {
-      var dataTable = this.model.getv("data"),
-        R = dataTable.getNumberOfRows(),
-        jMeasure = dataTable.model.attributes.get(this.model.getv("measure")).ordinal,
-        getValue = function(k) {
-          var v = dataTable.getValue(k, jMeasure);
-          return !isNaN(v) && v != null ? v : null;
-        },
-        i;
+      var dataTable = this.model.getv("data");
+      var R = dataTable.getNumberOfRows();
 
-      var value = null, vi;
+      var measure = this.model.getv("measure");
+      var jMeasure = dataTable.model.attributes.get(measure);
+      if (!jMeasure || !jMeasure.ordinal) {
+        window.alert("Invalid measure!");
+        return;
+      }
+
+      var measureOrdinal = jMeasure.ordinal;
+
+      var getValue = function(k) {
+        var v = dataTable.getValue(k, measureOrdinal);
+          return !isNaN(v) && v != null ? v : null;
+      };
+      var i;
+
+      var value = null;
       var op = this.model.getv("operation").toUpperCase();
 
       switch(op) {
         case "MAX":
-          for(i = 0; i < R; i++)
-            if((vi = getValue(i)) != null)
+          for (i = 0; i < R; i++) {
+            var vi = getValue(i);
+
+            if (vi != null) {
               value = value == null ? vi : Math.max(value, vi);
+            }
+          }
+
           break;
 
         case "MIN":
-          for(i = 0; i < R; i++)
-            if((vi = getValue(i)) != null)
+          for (i = 0; i < R; i++) {
+            var vi = getValue(i);
+
+            if (vi != null) {
               value = value == null ? vi : Math.min(value, vi);
+            }
+          }
+
           break;
 
         case "AVG":
-          var total = value = 0;
+          var total = 0;
+
           if(R) {
-            for(i = 0; i < R; i++)
-              if((vi = getValue(i)) != null)
+            for (i = 0; i < R; i++) {
+              var vi = getValue(i);
+
+              if (vi != null) {
                 total += vi;
+              }
+            }
+
             value = total / R;
+          } else {
+            value = 0;
           }
           break;
+
+        default:
+          window.alert("Invalid operation!");
+          value = undefined;
       }
 
       return value;
